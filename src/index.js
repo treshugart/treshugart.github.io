774710b46ba514c4cc1868066190106480a50333
Trey Shugart <treshugart@gmail.com>
2017-07-26 16:43:22 +1000

    Add meta tag.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /** @jsx h */
 
-import { Component, define } from 'skatejs/es-latest';
+import { Component, define, props } from 'skatejs/es-latest';
 import { h } from './utils';
 import { themed } from './utils/swc';
 import { Body } from './comps';
@@ -12,14 +12,34 @@ const Theme = themed({
   grid: 10
 });
 
+const Head = define(class extends Component {
+  static props = { content: props.array }
+  get renderRoot () {
+    return document.head;
+  }
+  renderCallback ({ content }) {
+    return content;
+  }
+});
+
 export default define(class extends Component {
   renderCallback () {
     return (
-      <Theme>
-        <Body>
-          <Page />
-        </Body>
-      </Theme>
+      <html>
+        <head>
+          <meta attrs={{
+            name: 'google-site-verification',
+            content: 'RQC7x29gWdBQabk-N2fbERaNjGckTk7zhDWLcpLw1e0'
+          }} />
+        </head>
+        <body>
+          <Theme>
+            <Body>
+              <Page />
+            </Body>
+          </Theme>
+        </body>
+      </html>
     );
   }
 });
