7a4da7720c315a6232187481186aa7d7392a0af3
Trey Shugart <treshugart@gmail.com>
2017-07-29 15:15:28 +1000

    Add meta tag.

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ require("babel-register")({
 const fs = require("fs");
 const render = require("@skatejs/ssr");
 const App = require("./src").default;
-const scripts = `<script src="https://unpkg.com/@webcomponents/webcomponentsjs"></script>`;
+const head = `
+  <head>
+    <meta name="google-site-verification" content="RQC7x29gWdBQabk-N2fbERaNjGckTk7zhDWLcpLw1e0">
+    <script src="https://unpkg.com/@webcomponents/webcomponentsjs"></script>
+  </head>
+`;
 
 render(new App()).then(r =>
-  fs.writeFile("index.html", `${scripts}${r}`, () => {})
+  fs.writeFile("index.html", `${head}${r}`, () => {})
 );
